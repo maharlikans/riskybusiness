4ad4a5cb0fee35c734d58d15e86be78b0cf52ed5
Joseph Pecoraro <jpecoraro342@ufl.edu>
2014-02-12 13:34:00 -0500

    First Project Commit Organized Project Contents (Java.com.package for files)

--- RiskyBusinessProject/RiskyBusiness/src/main/slothproductions/com/model/Board.java
+++
@@ -1,9 +0,0 @@
-public class Board {
-	protected Set<Hexagon> hexgagons;
-	protected ArrayList<Player> players;
-	protected HashMap<DiceRoll,Resource> rolls_resources;
-	protected HashMap<Player,Resource> players_resources;
-	protected Calendar start_timestamp;
-	protected Iterator<Player> current_player;
-
-}
