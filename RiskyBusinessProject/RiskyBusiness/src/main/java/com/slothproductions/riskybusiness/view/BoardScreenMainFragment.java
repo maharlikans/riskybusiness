4650f1267c244dbd05a0d8343a70cb57f47b0815
Joseph Pecoraro <jpecoraro342@ufl.edu>
2014-02-19 16:45:42 -0500

    Adjusted Board Hex Indices

--- RiskyBusinessProject/RiskyBusiness/src/main/java/com/slothproductions/riskybusiness/view/BoardScreenMainFragment.java
+++ RiskyBusinessProject/RiskyBusiness/src/main/java/com/slothproductions/riskybusiness/view/BoardScreenMainFragment.java
@@ -1,10 +1,13 @@
 package com.slothproductions.riskybusiness.view;
 
+import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.View.R;
 
@@ -21,6 +24,12 @@ public class BoardScreenMainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_board_screen, parent, false);
+
+        RelativeLayout mHexParent = (RelativeLayout)v.findViewById(R.id.hexParent);
+
+        ImageView iv = (ImageView)mHexParent.getChildAt(0);
+        iv.setColorFilter(Color.BLUE);
+
         return v;
     }
 
