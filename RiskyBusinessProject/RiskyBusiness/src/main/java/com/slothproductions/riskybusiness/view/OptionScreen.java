328fe5dd0054843f92c7e7c0561e2e33c8603464
Jacob Lehman <jlehman@ufl.edu>
2014-02-20 17:47:20 -0500

    Basic Option Screen files added

---
+++ RiskyBusinessProject/RiskyBusiness/src/main/java/com/slothproductions/riskybusiness/view/OptionScreen.java
@@ -0,0 +1,25 @@
+package com.slothproductions.riskybusiness.view;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.View.R;
+/**
+ * Created by Jacob on 2/20/14.
+ */
+public class OptionScreen extends Activity {
+
+
+
+        protected void  OnCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_option_screen);
+        }
+}
