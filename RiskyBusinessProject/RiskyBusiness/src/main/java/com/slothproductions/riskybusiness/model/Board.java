c2119911fb0b39a01187d8bd364be871065ae696
jonathanmclaus <jonathanmclaus@gmail.com>
2014-03-21 02:30:46 -0400

    Edge, Vertex, fixed Roll generation

--- RiskyBusinessProject/RiskyBusiness/src/main/java/com/slothproductions/riskybusiness/model/Board.java
+++ RiskyBusinessProject/RiskyBusiness/src/main/java/com/slothproductions/riskybusiness/model/Board.java
@@ -5,43 +5,6 @@ import android.util.Log;
 import java.util.*;
 
 public class Board {
-    protected class PlayerAccounting {
-        protected class ImmutablePlayerAccounting {
-            public int resource(Resource r) {
-                return resources.get(r);
-            }
-
-            public ArrayList<Edge.ImmutableEdge> getEdges() {
-                return immutableEdges;
-            }
-
-            public ArrayList<Vertex.ImmutableVertex> getVertices() {
-                return immutableVertices;
-            }
-
-            public ArrayList<MilitaryUnit.ImmutableMilitaryUnit> getMilitaryUnit() {
-                return immutableMilitaryUnits;
-            }
-        }
-
-        protected PlayerAccounting() {
-            resources = new HashMap<Resource, Integer>();
-            immutableEdges = new ArrayList<Edge.ImmutableEdge>();
-            immutableVertices = new ArrayList<Vertex.ImmutableVertex>();
-            immutableMilitaryUnits = new ArrayList<MilitaryUnit.ImmutableMilitaryUnit>();
-            edges = new ArrayList<Edge>();
-            vertices = new ArrayList<Vertex>();
-            militaryUnits = new ArrayList<MilitaryUnit>();
-        }
-
-        private Map<Resource, Integer> resources;
-        private ArrayList<Edge.ImmutableEdge> immutableEdges;
-        private ArrayList<Vertex.ImmutableVertex> immutableVertices;
-        private ArrayList<MilitaryUnit.ImmutableMilitaryUnit> immutableMilitaryUnits;
-        private ArrayList<Edge> edges;
-        private ArrayList<Vertex> vertices;
-        private ArrayList<MilitaryUnit> militaryUnits;
-    }
 
     private final String TAG = "BOARDDATA";
     private boolean locked = false;
@@ -49,21 +12,9 @@ public class Board {
     //hexes was changed to public because i need to access it somehow
     public List<Hex> hexes;
     protected List<Vertex> vertices;
-    protected List<MilitaryUnit> militaryUnits;
     protected List<Edge> edges;
-    protected List<Player> players;
     protected List<ArrayList<Hex>> diceRolls;
-
-    private Map<Player, PlayerAccounting> playerAccounting;
-
-    protected void addHex(Hex h) {
-        if (!locked) {
-            hexes.add(h);
-        } else {
-            throw new RuntimeException();
-        }
-    }
-
+    
     protected void addVertex(Vertex v) {
     	if (!locked) {
     		vertices.add(v);
@@ -93,14 +44,11 @@ public class Board {
         // hexes that make up the board.
         int radius = 3;
 
-
-        /* TODO: Loop to generate players */
-
         ArrayList<Resource> resources = generateResources();
 
         // Initializing values for the innermost ring with a single hex. This
         // is done manually as there is no pattern for this one.
-        Hex center = new Hex(resources.get(0), -1);
+        Hex center = new Hex(0, resources.get(0));
         hexes.add(center);
         int index = 1;
 
@@ -109,7 +57,7 @@ public class Board {
             int minIndex = index;
             int maxIndex = counter * (counter + 1) * 3;
             for (; index <= maxIndex; index++) {
-                Hex current = new Hex(resources.get(index), -1);
+                Hex current = new Hex(index, resources.get(index));
                 if ((index - minIndex) % counter == 0) { // On a corner of the ring
                     current.adjacent.add(hexes.get(innerIndex));
                     hexes.get(innerIndex).adjacent.add(current);
@@ -136,7 +84,65 @@ public class Board {
         ArrayList<Integer> rolls = generateRolls();
         Log.d(TAG, "Finished Generating Rolls");
         for (int counter = 0; counter <= 18; counter++)
-            hexes.get(counter).roll = rolls.get(counter);
+            hexes.get(counter).setRoll(rolls.get(counter));
+
+        Log.d(TAG, "Generating Vertices");
+        for (int i1 = 0; i1 <= 18; i1++) {
+            for (int i2 = 0; i2 < i1; i2++) {
+                for (int i3 = 0; i3 < i2; i3++) {
+                    Hex h1 = hexes.get(i1);
+                    Hex h2 = hexes.get(i2);
+                    Hex h3 = hexes.get(i3);
+                    if (h1.isAdjacent(i2) && h2.isAdjacent(i3) && h3.isAdjacent(i1)) {
+                        vertices.add(new Vertex(h1, h2, h3));
+                    }
+                }
+            }
+        }
+        for (int i1 = 0; i1 <= 18; i1++) {
+            for (int i2 = 0; i2 < i1; i2++) {
+                Hex h1 = hexes.get(i1);
+                Hex h2 = hexes.get(i2);
+                if (h1.isAdjacent(i2)) {
+                    int counter = 0;
+                    for (Vertex v : vertices) {
+                        if (v.isVertexOf(i1) && v.isVertexOf(i2))
+                            counter++;
+                    }
+                    for (int adding = 0; adding < 2 - counter; adding++)
+                        vertices.add(new Vertex(h1, h2, null));
+                }
+            }
+        }
+        for (int i1 = 0; i1 <= 18; i1++) {
+            Hex h1 = hexes.get(i1);
+            int counter = 0;
+            for (Vertex v : vertices) {
+                if (v.isVertexOf(i1))
+                    counter++;
+            }
+            for (int adding = 0; adding < 6 - counter; adding++)
+                vertices.add(new Vertex(h1, null, null));
+        }
+        Log.d(TAG, "Finished Generating Vertices");
+
+        Log.d(TAG, "Generating Edges");
+        for (int i1 = 0; i1 <= vertices.size(); i1++) {
+            for (int i2 = 0; i2 < i1; i2++) {
+                Vertex v1 = vertices.get(i1);
+                Vertex v2 = vertices.get(i2);
+                if (v1.isAdjacent(v2)) {
+                    ArrayList<Hex> shared = new ArrayList<Hex>();
+                    for (Hex h : v1.hexagons) {
+                        if (v2.hexagons.contains(h))
+                            shared.add(h);
+                    }
+                    edges.add(new Edge(shared.get(0), shared.get(1), v1, v2));
+                }
+            }
+        }
+        Log.d(TAG, "Finished Generating Edges");
+
     }
 
     /**
@@ -146,7 +152,7 @@ public class Board {
     ArrayList<Resource> generateResources() {
         ArrayList<Resource> resources = new ArrayList<Resource>(19);
 
-        resources.add(Resource.DESERT);
+        resources.add(Resource.GOLD);
 
         for (int i = 0; i < 3; i++) {
             resources.add(Resource.BRICK);
@@ -165,57 +171,43 @@ public class Board {
 
     ArrayList<Integer> generateRolls() {
         int[] rollsArray = {
-                8, 9, 10, 2, 5, 3, 9, 10, 12, 11, 8, 4, 11, 3, 6, 4, 6, 5
+                2, 3, 3, 4, 4, 5, 5, 9, 9, 10, 10, 11, 11, 12
         };
 
-        ArrayList<Integer> rolls = new ArrayList<Integer>();
-        for (int i : rollsArray)
-            rolls.add(i);
-        Collections.shuffle(rolls);
-        rolls.add(0, -1);
-
-        for (int i = 0; i <19; i ++) {
-            if (hexes.get(i).type == Resource.DESERT){
-                int temp = rolls.get(i);
-                rolls.set(0, temp);
-                rolls.set(i, -1);
-                Log.d(TAG, "Desert Roll Value Added");
+        ArrayList<Integer> invalid = new ArrayList<Integer>();
+        ArrayList<Integer> sixOrEight = new ArrayList<Integer>();
+        for (int counter = 0; counter < 4; counter++) {
+            int index = (int) (18 * Math.random());
+            while (invalid.contains(index)) {
+                index = (int) (18 * Math.random());
             }
+            invalid.add(index);
+            for (Hex h : hexes.get(index).adjacent)
+                invalid.add(h.index);
         }
 
-        return rolls;
-        /*
-        int[] ret = new int[19];
+        int goldIndex = 0;
+        for (Hex h : hexes)
+            if (h.type == Resource.GOLD)
+                goldIndex = h.index;
 
-        Log.d(TAG, "For Loop Entered");
-        for (int counter = 0; counter <= 18; counter++) {
-            if (hexes.get(counter).type == Resource.DESERT) {
-                ret[counter] = -1;
+
+        ArrayList<Integer> ret = new ArrayList<Integer>();
+        int arrayCounter = 0;
+        for (int counter = 0; counter < 18; counter++) {
+            int listIndex = sixOrEight.lastIndexOf(counter);
+            if (listIndex != -1) {
+                if (listIndex < sixOrEight.size() / 2)
+                    ret.add(6);
+                else
+                    ret.add(8);
+            } else if (goldIndex == counter) {
+                ret.add(7);
             } else {
-                //Error is somewhere in Here
-                while (true) {
-                    int current = rolls.remove(0);
-                    boolean adjacent = false;
-                    if (current == 6 || current == 8)
-                        for (Hex h : hexes.get(counter).adjacent) {
-                            if (h.roll == 6 || h.roll == 8)
-                                adjacent = true;
-                        }
-                    if (!adjacent) {
-                        ret[counter] = current;
-                        hexes.get(counter).roll = current;
-                        break;
-                    } else {
-                        rolls.add(current);
-                    }
-                    Log.d(TAG, "While Loop Continuing");
-                }
-                Collections.shuffle(rolls);
-
+                ret.add(rollsArray[arrayCounter]);
+                arrayCounter++;
             }
         }
-        Log.d(TAG, "For Loop Finished");
         return ret;
-        */
     }
 }
